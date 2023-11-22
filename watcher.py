1c5b4c937e397a516edc77c96e55fd72271283c9
clorin <clorin@student.42.fr>
2023-11-22 13:15:38 +0100

    add time slot

--- watcher.py
+++ watcher.py
@@ -21,15 +21,15 @@ def get_params():
     """Get the last params saved on the disk. Return None if no params saved."""
     try:
         with open("./params.pkl", "rb") as file:
-            url, team_id, token, api = pickle.load(file)
-            return (url, team_id, token, api)
+            url, team_id, token, api, forktime = pickle.load(file)
+            return (url, team_id, token, api, forktime)
     except:
         return (None, None, None, None)
 
-def save_params(url, team_id, token, api):
+def save_params(url, team_id, token, api, forktime):
     """Save params on the disk."""
     with open("./params.pkl", "wb+") as file:
-        pickle.dump((url, team_id, token, api), file)
+        pickle.dump((url, team_id, token, api, forktime), file)
 
 def notify(title, message):
     """Send a desktop notification."""
@@ -43,7 +43,7 @@ def notify(title, message):
         n = notify2.Notification(title, message)
         n.show()
 
-def get_slots():
+def get_slots(project_url, team_id, days_to_begin, days_to_watch):
     """Send a request to 42 intra."""
     today = datetime.date.today()
     start = today + datetime.timedelta(days=days_to_begin)
@@ -57,7 +57,7 @@ def get_slots():
         }
     )
     response = request.json()
-    print(response)
+    # print(response)
     if ("error" in response):
         print("Unable to connect, check the token!")
         exit(0)
@@ -70,13 +70,23 @@ def call_api(message):
     except Exception as e:
         print(e)
 
+def get_time(slot):
+    # Récupération de l'heure de début
+    start_datetime_str = slot[0]['start']
+    start_datetime = datetime.datetime.fromisoformat(start_datetime_str[:-6])  # Conversion en objet datetime
+    start_time = start_datetime.time()  # Récupération de l'heure
+    return (start_time)
+
 # Check if we previously saved params
-previous_url, previous_team_id, previous_token, previous_api = get_params()
+previous_url, previous_team_id, previous_token, previous_api, previous_forktime = get_params()
 
 url_placeholder = ""
 team_id_placeholder = ""
 token_placeholder = ""
 api_placeholder = ""
+forktime_placeholder_start = ""
+forktime_placeholder_end = ""
+
 if (not previous_url is None):
     url_placeholder = f" (default {previous_url})"
 if (not previous_team_id is None):
@@ -85,6 +95,9 @@ if (not previous_token is None):
     token_placeholder = f" (default {previous_token})"
 if (not previous_api is None):
     api_placeholder = f" (default {previous_api})"
+if (not previous_forktime is None):
+    forktime_placeholder_start = f" (default {previous_forktime[0].strftime('%H:%M')})"
+    forktime_placeholder_end = f" (default {previous_forktime[1].strftime('%H:%M')})"
 
 # Get variables
 print("Refer to ReadMe.md to help you find the following data.")
@@ -115,17 +128,37 @@ try:
 except:
     days_to_begin = 0
 
-save_params(project_url, team_id, token, api)
+start_time_str = input(f"Please enter the start time (in HH:MM format){forktime_placeholder_start}: ")
+if start_time_str == "":
+    start_time_str = previous_forktime[0].strftime('%H:%M')
+end_time_str = input(f"Please enter the end time (in HH:MM format){forktime_placeholder_end}: ")
+if end_time_str == "":
+    end_time_str = previous_forktime[1].strftime('%H:%M')
+# Convert the input strings to time objects
+try:
+    forktime = (datetime.datetime.strptime(start_time_str, "%H:%M").time(),
+         datetime.datetime.strptime(end_time_str, "%H:%M").time())
+except ValueError:
+    print("Incorrect time format. Please make sure to use the HH:MM format.")
+    # Handle the error or exit the program according to your needs
+    forktime = (datetime.time(0,0), datetime.time(23,59)) 
+save_params(project_url, team_id, token, api, forktime)
 
 notify("Running", "You'll be notified when an open slot is found.")
 if api:
     call_api("Starting 42 search slot correction for [" + project_url + "]")
 while True:
     print("Checking...")
-    slots = get_slots()
+    slots = get_slots(project_url, team_id, days_to_begin, days_to_watch)
     if (len(slots) > 0):
-        print("Slot found.")
-        notify("Correction found!", "An open slot has been found for " + project_url)
-        if api:
-            call_api("42: Correction Found for " + project_url)
+        # forktime = (datetime.time(12,30), datetime.time(16,0)) 
+        start_time = get_time(slots)
+        print(f"Slot found at {start_time}")
+        in_forktime = forktime[0] <= start_time <= forktime[1]
+        if in_forktime:
+            notify(f"Correction found!", "An open slot has been found for {project_url} at {start_time}")
+            if api:
+                call_api(f"42: Correction Found for {project_url} at {start_time}")
+        else:
+            print(f" is not between {forktime[0]} and {forktime[1]}")
     time.sleep(WAIT_TIME)
